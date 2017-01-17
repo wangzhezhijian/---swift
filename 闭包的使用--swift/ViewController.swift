5b6a6f05440d093631a39b512aace438d7abb758
AISION <lisiguang@wangzhongkaidemacbook-pro.local>
2017-01-17 09:34:33 +0800

    Initial Commit

---
+++ 闭包的使用--swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  闭包的使用--swift
+//
+//  Created by AISION on 17/1/17.
+//  Copyright © 2017年 AISION. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
